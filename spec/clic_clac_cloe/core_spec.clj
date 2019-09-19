e85b4119072e3ab5d0a390655d0014ef92acd7e2
crkubiak <charles@8thlight.com>
2019-09-19 11:43:49 -0500

    initial commit

---
+++ spec/clic_clac_cloe/core_spec.clj
@@ -0,0 +1,7 @@
+(ns clic-clac-cloe.core-spec
+  (:require [speclj.core :refer :all]
+            [clic-clac-cloe.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/clic_clac_cloe/core.clj
@@ -0,0 +1,5 @@
+(ns clic-clac-cloe.core)
+
+(defn -main
+  [& args]
+  (println "Hello World"))
